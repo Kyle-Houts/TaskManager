98355f28e410c3902d0f6d1eaeb92630c49b3622
Kyle Houts <kyle.elizabeth.houts@gmail.com>
2018-09-14 08:01:43 -0500

    Initial Commit

---
+++ TaskManager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TaskManager
+//
+//  Created by Kyle Houts on 9/14/18.
+//  Copyright © 2018 Kyle Houts. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
